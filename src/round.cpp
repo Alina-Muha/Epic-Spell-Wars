ab9c50e2c7d39eecaa9739e486f9dc39d9001231
ivlev.nikit <ivlev.nikit@gmail.com>
2022-03-19 00:38:43 +0300

    Написал card, осталось чуть чуть дописать round, начал писать

--- src/round.cpp
+++ src/round.cpp
@@ -1,6 +1,75 @@
-#include "../include/round.h"
+#include "../include/Round.h"
+#include "algorithm"
 namespace round{
-    void round::distribute_cards() {}
-    void round::move_cards_from_hand_to_discard() {}
-    void round::playing_cards() {}
+
+    bool compare_two_players(player::Player& a,player::Player& b){
+        if(a.get_cards().size()<b.get_cards().size()){
+            return true;
+        }
+        if(a.get_cards().size()>b.get_cards().size()){
+            return false;
+        }
+        int priority_of_the_turn_a=0;
+        int priority_of_the_turn_b=0;
+        for(auto i : a.spell){
+            priority_of_the_turn_a=std::max(i, priority_of_the_turn_a);
+            priority_of_the_turn_a=std::max(i, priority_of_the_turn_a);
+        }
+        if(priority_of_the_turn_a>priority_of_the_turn_b){
+            return true;
+        }
+        if(priority_of_the_turn_a<priority_of_the_turn_b){
+            return false;
+        }
+    }
+
+    void Round::sort_priority_of_the_turn() {
+        std::sort(alive_players.begin(), alive_players.end(), compare_two_players);
+    }
+
+    void Round::distribute_cards() {
+        bool flag_of_end_of_deck=false;
+        for(auto i: alive_players){
+            if(flag_of_end_of_deck){
+                break;
+            }
+            while(i->get_cards().size()<number_of_cards_in_hand){
+                if(main_deck.size()!=0) {
+                    i->change_cards(main_deck.back());
+                    main_deck.pop_back();
+                }else{
+                    flag_of_end_of_deck=true;
+                    break;
+                }
+            }
+        }
+    }
+
+    void play_cards(player::Player& gamer){
+        for(auto i : spell){
+            i.first.do_card_effects(i.second);
+        }
+        spell.clear();
+    }
+
+    void Round::play_circle() {
+        for(auto it = alive_players.begin(); it!=alive_players.end();it++){
+            auto b = *it;
+            if(b->get_lives()<=0){
+                alive_players.erase(it);
+                b->spell.clear();
+                continue;
+            }
+            play_cards(b);
+        }
+    }
+
+    void Round::play_round() {
+        while(alive_players.size()!=1) {
+            //тут как то еще надо подождать чтобы все живые свои заклинания скинули
+            sort_priority_of_the_turn();
+            play_circle();
+        }
+    }
+
 }
