e401859f8a3cd009fc404ead2bbaba204f0f8ed1
Alina-Muha <AlinaMyxa2002@yandex.ru>
2022-05-24 00:27:03 +0300

    controller + client

--- src/client.cpp
+++ src/client.cpp
@@ -12,9 +12,26 @@ Client::Client() {
 
 void Client::connect(char* ip, int port) {
     socket->connectToHost(ip, port);
+
 }
 
+void send_json() {
 
+}
+
+void get_json(){
+
+}
+
+void Client::slotReadyRead() {
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_2);
+    if (in.status() == QDataStream::Ok) {
+        //Qstring name;
+        //in >> name; // ????
+
+    }
+}
 
 
 
