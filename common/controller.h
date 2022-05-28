a5e08e776b960d03d8da62765d440310c361b1dc
Alina-Muha <AlinaMyxa2002@yandex.ru>
2022-05-29 01:59:12 +0300

    getters and constructors in controller

--- common/controller.h
+++ common/controller.h
@@ -6,46 +6,6 @@
 #include<QJsonObject>
 #include<QJsonValue>
 
-class JsonCard
-{
-public:
-    JsonCard(QJsonObject jObj);
-    QJsonObject toJsonObject();
-private:
-    QString type_of_spell;
-    int number;
-    bool need_to_roll_the_dice;
-};
-
-class CardsJsonController
-{
-public:
-    CardsJsonController();
-    QByteArray list();
-    void append(QByteArray json);
-    void clear();
-private:
-    QList<JsonCard> items;
-};
-
-class JsonMessage{
-public:
-private:
-
-};
-
-#endif // CONTROLLER_H
-
-======================================================
-
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-
-#include <QJsonDocument>
-#include<QJsonArray>
-#include<QJsonObject>
-#include<QJsonValue>
-
 namespace controller {
 
 class JsonCard
