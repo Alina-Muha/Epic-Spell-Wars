a695ffc4be56416fa595e605b793c308704a9003
mediana105 <st087217@student.spbu.ru>
2022-06-01 02:13:39 +0300

    imitation of game

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,6 +5,7 @@
 #include <QApplication>
 #include <iostream>
 #include <cassert>
+#include "card.h"
 int main(int argc, char *argv[])
 {
     round_of_game::Round round;
@@ -21,15 +22,133 @@ int main(int argc, char *argv[])
     round_ptr->alive_players.emplace_back(player_ptr_2);
     round_ptr->alive_players.emplace_back(player_ptr_3);
     round_ptr->alive_players.emplace_back(player_ptr_4);
+
+
     player_ptr_1.get()->change_lives(21);
     player_ptr_2.get()->change_lives(22);
     player_ptr_3.get()->change_lives(23);
     player_ptr_4.get()->change_lives(10);
+
+
     for (auto player : round_ptr.get()->get_alive_players()){
         std::cout << player.get()->get_name() << ": " << player.get()->get_lives() << '\n';
     }
-    card_functions::CardFunctions card;
-    std::cout << "Strongest player is: " << card.get_the_strongest_player(round_ptr).get()->get_name() << '\n';
-    std::cout << "Weakest player is: " << card.get_the_weakest_player(round_ptr).get()->get_name() << '\n';
+    card_functions::CardFunctions card_functions;
+    std::cout << "Strongest player is: " << card_functions.get_the_strongest_player(round_ptr).get()->get_name() << '\n';
+    std::cout << "Weakest player is: " << card_functions.get_the_weakest_player(round_ptr).get()->get_name() << '\n';
+
+
+    card::Card card_for_player1_1(1, card::Card::type::elemental, card::Card::type_of_spell_component::delivery, 1);
+    card::Card card_for_player1_2(10, card::Card::type::elemental, card::Card::type_of_spell_component::delivery, 2);
+    card::Card card_for_player1_3(11, card::Card::type::illusion, card::Card::type_of_spell_component::delivery, 3);
+    card::Card card_for_player1_4(12, card::Card::type::primal, card::Card::type_of_spell_component::delivery, 4);
+    card::Card card_for_player1_5(13, card::Card::type::illusion, card::Card::type_of_spell_component::delivery, 5);
+    std::shared_ptr<card::Card> card_for_player1_1_ptr = std::make_shared<card::Card>(card_for_player1_1);
+    std::shared_ptr<card::Card> card_for_player1_2_ptr = std::make_shared<card::Card>(card_for_player1_2);
+    std::shared_ptr<card::Card> card_for_player1_3_ptr = std::make_shared<card::Card>(card_for_player1_3);
+    std::shared_ptr<card::Card> card_for_player1_4_ptr = std::make_shared<card::Card>(card_for_player1_4);
+    std::shared_ptr<card::Card> card_for_player1_5_ptr = std::make_shared<card::Card>(card_for_player1_5);
+
+
+    card::Card card_for_player2_1(14, card::Card::type::dark, card::Card::type_of_spell_component::delivery, 1);
+    card::Card card_for_player2_2(2, card::Card::type::dark, card::Card::type_of_spell_component::delivery, 2);
+    card::Card card_for_player2_3(3, card::Card::type::elemental, card::Card::type_of_spell_component::delivery, 3);
+    card::Card card_for_player2_4(4, card::Card::type::elemental, card::Card::type_of_spell_component::delivery, 4);
+    card::Card card_for_player2_5(5, card::Card::type::primal, card::Card::type_of_spell_component::delivery, 5);
+    std::shared_ptr<card::Card> card_for_player2_1_ptr = std::make_shared<card::Card>(card_for_player2_1);
+    std::shared_ptr<card::Card> card_for_player2_2_ptr = std::make_shared<card::Card>(card_for_player2_2);
+    std::shared_ptr<card::Card> card_for_player2_3_ptr = std::make_shared<card::Card>(card_for_player2_3);
+    std::shared_ptr<card::Card> card_for_player2_4_ptr = std::make_shared<card::Card>(card_for_player2_4);
+    std::shared_ptr<card::Card> card_for_player2_5_ptr = std::make_shared<card::Card>(card_for_player2_5);
+
+
+    card::Card card_for_player3_1(6, card::Card::type::primal, card::Card::type_of_spell_component::delivery, 1);
+    card::Card card_for_player3_2(7, card::Card::type::primal, card::Card::type_of_spell_component::delivery, 2);
+    card::Card card_for_player3_3(8, card::Card::type::dark, card::Card::type_of_spell_component::delivery, 3);
+    card::Card card_for_player3_4(9, card::Card::type::dark, card::Card::type_of_spell_component::delivery, 4);
+    card::Card card_for_player3_5(1, card::Card::type::primal, card::Card::type_of_spell_component::quality, 5);
+    std::shared_ptr<card::Card> card_for_player3_1_ptr = std::make_shared<card::Card>(card_for_player3_1);
+    std::shared_ptr<card::Card> card_for_player3_2_ptr = std::make_shared<card::Card>(card_for_player3_2);
+    std::shared_ptr<card::Card> card_for_player3_3_ptr = std::make_shared<card::Card>(card_for_player3_3);
+    std::shared_ptr<card::Card> card_for_player3_4_ptr = std::make_shared<card::Card>(card_for_player3_4);
+    std::shared_ptr<card::Card> card_for_player3_5_ptr = std::make_shared<card::Card>(card_for_player3_5);
+
+
+    card::Card card_for_player4_1(10, card::Card::type::primal, card::Card::type_of_spell_component::quality, 1);
+    card::Card card_for_player4_2(11, card::Card::type::primal, card::Card::type_of_spell_component::quality, 2);
+    card::Card card_for_player4_3(12, card::Card::type::dark, card::Card::type_of_spell_component::quality, 3);
+    card::Card card_for_player4_4(13, card::Card::type::primal, card::Card::type_of_spell_component::quality, 4);
+    card::Card card_for_player4_5(2, card::Card::type::elemental, card::Card::type_of_spell_component::quality, 5);
+    std::shared_ptr<card::Card> card_for_player4_1_ptr = std::make_shared<card::Card>(card_for_player4_1);
+    std::shared_ptr<card::Card> card_for_player4_2_ptr = std::make_shared<card::Card>(card_for_player4_2);
+    std::shared_ptr<card::Card> card_for_player4_3_ptr = std::make_shared<card::Card>(card_for_player4_3);
+    std::shared_ptr<card::Card> card_for_player4_4_ptr = std::make_shared<card::Card>(card_for_player4_4);
+    std::shared_ptr<card::Card> card_for_player4_5_ptr = std::make_shared<card::Card>(card_for_player4_5);
+
+    player_ptr_1.get()->add_card(card_for_player1_1_ptr);
+    player_ptr_1.get()->add_card(card_for_player1_2_ptr);
+    player_ptr_1.get()->add_card(card_for_player1_3_ptr);
+    player_ptr_1.get()->add_card(card_for_player1_4_ptr);
+    player_ptr_1.get()->add_card(card_for_player1_5_ptr);
+
+    player_ptr_2.get()->add_card(card_for_player2_1_ptr);
+    player_ptr_2.get()->add_card(card_for_player2_2_ptr);
+    player_ptr_2.get()->add_card(card_for_player2_3_ptr);
+    player_ptr_2.get()->add_card(card_for_player2_4_ptr);
+    player_ptr_2.get()->add_card(card_for_player2_5_ptr);
+
+    player_ptr_3.get()->add_card(card_for_player3_1_ptr);
+    player_ptr_3.get()->add_card(card_for_player3_2_ptr);
+    player_ptr_3.get()->add_card(card_for_player3_3_ptr);
+    player_ptr_3.get()->add_card(card_for_player3_4_ptr);
+    player_ptr_3.get()->add_card(card_for_player3_5_ptr);
+
+    player_ptr_4.get()->add_card(card_for_player4_1_ptr);
+    player_ptr_4.get()->add_card(card_for_player4_2_ptr);
+    player_ptr_4.get()->add_card(card_for_player4_3_ptr);
+    player_ptr_4.get()->add_card(card_for_player4_4_ptr);
+    player_ptr_4.get()->add_card(card_for_player4_5_ptr);
+
+    player_ptr_1.get()->add_card_to_spell(card_for_player1_1_ptr);
+    player_ptr_1.get()->add_card_to_spell(card_for_player1_2_ptr);
+    player_ptr_1.get()->add_card_to_spell(card_for_player1_3_ptr);
+
+    player_ptr_2.get()->add_card_to_spell(card_for_player2_1_ptr);
+    player_ptr_2.get()->add_card_to_spell(card_for_player2_2_ptr);
+    player_ptr_2.get()->add_card_to_spell(card_for_player2_3_ptr);
+
+    player_ptr_3.get()->add_card_to_spell(card_for_player3_1_ptr);
+    player_ptr_3.get()->add_card_to_spell(card_for_player3_2_ptr);
+    player_ptr_3.get()->add_card_to_spell(card_for_player3_3_ptr);
+
+    player_ptr_4.get()->add_card_to_spell(card_for_player4_1_ptr);
+    player_ptr_4.get()->add_card_to_spell(card_for_player4_2_ptr);
+    player_ptr_4.get()->add_card_to_spell(card_for_player4_3_ptr);
+
+    std::cout << player_ptr_1.get()->get_name() << ":\n" << "achane: " << card_functions.get_achane_num_in_spell(player_ptr_1) << ", dark: " <<
+                 card_functions.get_dark_num_in_spell(player_ptr_1) << ", illusion: " <<
+                 card_functions.get_illusion_num_in_spell(player_ptr_1) << ", primal: " <<
+                 card_functions.get_primal_num_in_spell(player_ptr_1) << ", elemental: " <<
+                 card_functions.get_elemental_num_in_spell(player_ptr_1) << "; unique: " << card_functions.unique_types_in_spell(player_ptr_1) << '\n';
+
+    std::cout << player_ptr_2.get()->get_name() << ":\n" << "achane: " << card_functions.get_achane_num_in_spell(player_ptr_2) << ", dark: " <<
+                 card_functions.get_dark_num_in_spell(player_ptr_2) << ", illusion: " <<
+                 card_functions.get_illusion_num_in_spell(player_ptr_2) << ", primal: " <<
+                 card_functions.get_primal_num_in_spell(player_ptr_2) << ", elemental: " <<
+                 card_functions.get_elemental_num_in_spell(player_ptr_2) << "; unique: " << card_functions.unique_types_in_spell(player_ptr_2) << '\n';
+
+    std::cout << player_ptr_3.get()->get_name() << ":\n" << "achane: " << card_functions.get_achane_num_in_spell(player_ptr_3) << ", dark: " <<
+                 card_functions.get_dark_num_in_spell(player_ptr_3) << ", illusion: " <<
+                 card_functions.get_illusion_num_in_spell(player_ptr_3) << ", primal: " <<
+                 card_functions.get_primal_num_in_spell(player_ptr_3) << ", elemental: " <<
+                 card_functions.get_elemental_num_in_spell(player_ptr_3) << "; unique: " << card_functions.unique_types_in_spell(player_ptr_3) << '\n';
+
+    std::cout << player_ptr_4.get()->get_name() << ":\n" << "achane: " << card_functions.get_achane_num_in_spell(player_ptr_4) << ", dark: " <<
+                 card_functions.get_dark_num_in_spell(player_ptr_4) << ", illusion: " <<
+                 card_functions.get_illusion_num_in_spell(player_ptr_4) << ", primal: " <<
+                 card_functions.get_primal_num_in_spell(player_ptr_4) << ", elemental: " <<
+                 card_functions.get_elemental_num_in_spell(player_ptr_4) << "; unique: " << card_functions.unique_types_in_spell(player_ptr_4) << '\n';
+
+
     return 0;
 }
