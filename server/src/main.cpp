32cc3dfb63acf089624de24359bcc993245a9e28
Alina-Muha <AlinaMyxa2002@yandex.ru>
2022-06-05 13:57:59 +0300

    final

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,9 +1,11 @@
 #include "server.h"
+#include "server_window.h"
 #include <QApplication>
-#include <iostream>
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
-  server::Server play_server(1234);
+  server_window w(nullptr);
+  w.setWindowTitle("server");
+  w.show();
   return a.exec();
 }
